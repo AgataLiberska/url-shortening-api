4d0ab84ac627d5e41d54f8c8f7e978db0c0fc43d
Agata Liberska <liberska@gmail.com>
2021-04-30 11:48:09 +0100

    extract data fetching to custom hook

--- src/components/ShortlyApp.js
+++ src/components/ShortlyApp.js
@@ -1,40 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ShortlyForm from './ShortlyForm';
 import LinkList from './LinkList';
-import shrtcode from '../API/shrtcode';
+import useUrls from '../hooks/useUrls';
 
-const useStateWithLocalStorage = localStorageKey => {
-    const [ value, setValue ] = useState(JSON.parse(localStorage.getItem(localStorageKey)) || [] );
-
-    useEffect(() => {
-        localStorage.setItem(localStorageKey, JSON.stringify(value))
-    }, [value, localStorageKey]);
-
-    return [value, setValue];
-}
 
 const ShortlyApp = () => {
-    const [ appError, setAppError ] = useState('');
-    const [ isLoading, setIsLoading ] = useState(false);
-    const [ results, setResults ] = useStateWithLocalStorage('results');
-
-    const submitUrl = async (url) => {
-        setIsLoading(true);
-        try {
-            setAppError('');
-            const {data} = await shrtcode.get('/shorten', { 
-                params: {
-                    url: url.trim()
-                }
-            })
-            setIsLoading(false);
-            setResults(results => [data, ...results]);
-
-        } catch (e) {
-            setIsLoading(false);
-            setAppError('Something went wrong. Check if URL is correct and try again.')
-        }
-    }
+    const { appError, isLoading, results, submitUrl, setResults } = useUrls();
 
     return (
         <>
